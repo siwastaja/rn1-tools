3237c4c6a862cdd983e628797f2d0b6060620f3a
Antti Alhonen <antti.alhonen@iki.fi>
2017-05-03 07:56:03 +0000

    relay script

--- relay.sh
+++ relay.sh
@@ -2,6 +2,10 @@
 
 brainfw="/home/hrst/rn1-tools/main.bin"
 motconfw="/home/hrst/rn1-tools/motcon.bin"
+prog="/home/hrst/rn1-tools/prog"
+mcprog="/home/hrst/rn1-tools/mcprog"
+udpser="/home/hrst/rn1-tools/udpserver"
+
 uart="/dev/serial0"
 
 while true
@@ -9,20 +13,23 @@ do
 	if [ -f "$brainfw" ]
 	then
 		echo "Reflashing main cpu..."
-		./prog $uart $brainfw s
+		$prog $uart $brainfw s
+		sleep 3
 		rm $brainfw
 	fi
 
 	if [ -f "$motconfw" ]
 	then
 		echo "Reflashing motor controller cpu 3..."
-		./mcprog $uart $motconfw 3
+		$mcprog $uart $motconfw 3
+		sleep 3
 		echo "Reflashing motor controller cpu 4..."
-		./mcprog $uart $motconfw 4
+		$mcprog $uart $motconfw 4
+		sleep 3
 		rm $motconfw
 	fi
 
 	echo "Starting udpserver..."
-	./udpserver $uart 22334
+	$udpser $uart 22334
 	sleep 1
 done
