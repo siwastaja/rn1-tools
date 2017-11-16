dbf760cfdb8beb5b78ef4519abfacfca2bd3c025
Antti Alhonen <antti.alhonen@iki.fi>
2017-11-16 11:31:11 +0200

    better programming script

--- p.sh
+++ p.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-brainfw="/home/hrst/rn1-tools/main.bin"
-motconfw="/home/hrst/rn1-tools/motcon.bin"
-brainfwtmp="/home/hrst/rn1-tools/main_tmp.bin"
-motconfwtmp="/home/hrst/rn1-tools/motcon_tmp.bin"
-prog="/home/hrst/rn1-tools/prog"
-mcprog="/home/hrst/rn1-tools/mcprog"
-udpser="/home/hrst/rn1-tools/udpserver"
+# This script reflashes main cpu and/or two motor controller CPUs
+# if the firmware files exist. The files are renamed to prevent
+# unnecessary flashing again.
+# If no files are present, nothing happens. For example, if you want,
+# you could run this script at every boot..
+
+
+
+# Fill in the motor controller indexes (1 to 4) in use
+# Order doesn't matter - both are reflashed
+motcona=1
+motconb=4
+
+tooldir="/home/hrst/rn1-tools"
+
+logfile="${tooldir}/program_log.txt"
+
+
+# Actual firmware filenames expected
+brainfw="${tooldir}/main.bin"
+motconfw="${tooldir}/motcon.bin"
+
+
+brainfwtmp="${tooldir}/main_tmp.bin"
+motconfwtmp="${tooldir}/motcon_tmp.bin"
+
+# Programming executables
+prog="${tooldir}/prog"
+mcprog="${tooldir}/mcprog"
 
 uart="/dev/serial0"
+
+	echo "Running programmer" > $logfile
+	echo "Running programmer"
+	date >> $logfile
 
 	if [ -f "$brainfwtmp" ]
 	then
 		rm $brainfwtmp
+	fi
+
+	if [ -f "$brainfw" ]
+	then
+		echo "Reflashing main cpu..."
+		echo "Reflashing main cpu..." >> $logfile
+		mv $brainfw $brainfwtmp
+		$prog $uart $brainfwtmp s >> $logfile
+		rm $brainfwtmp
+		sleep 3
 	fi
 
 	if [ -f "$motconfwtmp" ]
@@ -20,23 +56,16 @@ uart="/dev/serial0"
 		rm $motconfwtmp
 	fi
 
-	if [ -f "$brainfw" ]
-	then
-		echo "Reflashing main cpu..."
-		mv $brainfw $brainfwtmp
-		$prog $uart $brainfwtmp s
-		rm $brainfwtmp
-		sleep 3
-	fi
-
 	if [ -f "$motconfw" ]
 	then
-		echo "Reflashing motor controller cpu 3..."
+		echo "Reflashing motor controller cpu ${motcona}..."
+		echo "Reflashing motor controller cpu ${motcona}..." >> $logfile
 		mv $motconfw $motconfwtmp
-		$mcprog $uart $motconfwtmp 3
+		$mcprog $uart $motconfwtmp ${motcona} >> $logfile
 		sleep 4
-		echo "Reflashing motor controller cpu 4..."
-		$mcprog $uart $motconfwtmp 4
+		echo "Reflashing motor controller cpu ${motconb}..."
+		echo "Reflashing motor controller cpu ${motconb}..." >> $logfile
+		$mcprog $uart $motconfwtmp ${motconb} >> $logfile
 		rm $motconfwtmp
 		sleep 3
 	fi
